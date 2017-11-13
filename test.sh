5b0218b6a8dccb0a72e9f6339eeb06cc6b0ea52b
Jesse Cooke <jesse@jc00ke.com>
2017-11-12 18:02:37 -0800

    Expected & actual

--- test.sh
+++ test.sh
@@ -4,23 +4,25 @@ set -e
 __dir__="$(cd "$(dirname "$0")" && pwd)"
 
 test_input_doesnt_exist() {
-  local output="$($__dir__/cpp17csv doesnt-exist.csv c v o)"
-  if [ "Input file is missing" != "$output" ]
+  local expected="Input file is missing"
+  local actual="$($__dir__/cpp17csv doesnt-exist.csv c v o)"
+  if [ "$expected" != "$actual" ]
   then
     echo "${FUNCNAME[0]} failed"
-    echo "Expected: Input file is missing"
-    echo "Actual: $output"
+    echo "Expected: $expected"
+    echo "Actual: $actual"
     exit 1
   fi
 }
 
 test_input_is_empty() {
-  local output="$($__dir__/cpp17csv test-empty.csv c v o)"
-  if [ "Input file is empty" != "$output" ]
+  local expected="Input file is empty"
+  local actual="$($__dir__/cpp17csv test-empty.csv c v o)"
+  if [ "$expected" != "$actual" ]
   then
     echo "${FUNCNAME[0]} failed"
-    echo "Expected: Input file is empty"
-    echo "Actual: $output"
+    echo "Expected: $expected"
+    echo "Actual: $actual"
     exit 1
   fi
 }
