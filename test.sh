b420241cf98ece6010979fc6a2019d65e871cbf3
Jesse Cooke <jesse@jc00ke.com>
2017-11-12 18:14:05 -0800

    Extract assert_equal in shell tests

--- test.sh
+++ test.sh
@@ -6,39 +6,37 @@ __dir__="$(cd "$(dirname "$0")" && pwd)"
 test_input_doesnt_exist() {
   local expected="Input file is missing"
   local actual="$($__dir__/cpp17csv doesnt-exist.csv c v o)"
-  if [ "$expected" != "$actual" ]
-  then
-    echo "${FUNCNAME[0]} failed"
-    echo "Expected: $expected"
-    echo "Actual: $actual"
-    exit 1
-  fi
+
+  assert_equal "$expected" "$actual"
 }
 
 test_input_is_empty() {
   local expected="Input file is empty"
   local actual="$($__dir__/cpp17csv test-empty.csv c v o)"
-  if [ "$expected" != "$actual" ]
-  then
-    echo "${FUNCNAME[0]} failed"
-    echo "Expected: $expected"
-    echo "Actual: $actual"
-    exit 1
-  fi
+
+  assert_equal "$expected" "$actual"
 }
 
 test_headers_exclude_column() {
   local expected="Column name doesn't exist in the input file"
   local actual="$($__dir__/cpp17csv test-input.csv zebra v o)"
+
+  assert_equal "$expected" "$actual"
+}
+
+assert_equal() {
+  local expected="$1"
+  local actual="$2"
+
   if [ "$expected" != "$actual" ]
   then
-    echo "${FUNCNAME[0]} failed"
+    local called_by="${FUNCNAME[1]}"
+    echo "$called_by failed"
     echo "Expected: $expected"
     echo "Actual: $actual"
     exit 1
   fi
 }
-
 test_input_doesnt_exist
 test_input_is_empty
 test_headers_exclude_column
